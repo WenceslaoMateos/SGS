4dea34944258617b6d213817bb54f6f928477924
Wenceslao Mateos <wenceslaomateos@gmail.com>
2018-04-17 16:16:30 -0300

    Inutilidad de las practicas de gmt

--- GMT practicas/prueba2.bat
+++
@@ -1,3 +0,0 @@
-psxyz GEBCO2014_-31.0_30.0_-19.0_42.0_30Sec_ESRIASCII.asc -B1 -Bz1000+l"cosito" -BWSneZ+b+tmapitachoto -R-31/-19/30/42/-6000/0 -JM5i -JZ6i -p200/30 -P -Wthinnest -Glightgreen > map.ps
-psconvert -Tb map.ps
-map.bmp
--- GMT practicas/wen.bat
+++
@@ -1,5 +0,0 @@
-ECHO on
-grd2xyz datos31.0_30.0_-19.0_42.0.nc > map.ps
-psxyz datos.dat -B1 -Bz1000+l"cosito" -BWSneZ+b+tmapitachoto -R-170/170/-80/80/-6000/0 -JM5i -JZ6i -p200/30 -So0.0833333ub-5000 -P -Wthinnest -Glightgreen > map.ps
-gmt psconvert -Tb map.ps
-map.bmp
