9caed163e5d5bd0495e78be282eab2e31c1f71ec
Wen <wenceslaomateos@gmail.com>
2018-12-06 09:25:10 -0300

    Aplicación de estandares

--- ajaxData.php
+++ ajaxData.php
@@ -2,7 +2,7 @@
 $permiso = 0;
 $precisa_sesion = true;
 require('templates/coneccion.php');
-if (!empty($_REQUEST["barco"])) {
+if (!empty($_REQUEST['barco'])) {
     $campanias_query = mysqli_query($db, "SELECT * FROM campanias WHERE idbarcos = " . $_REQUEST['barco'] . ";");
     while ($campanias = mysqli_fetch_array($campanias_query)) {
         echo '<option value="' . $campanias['id'] . '">' . $campanias['nombre'] . '</option>';
